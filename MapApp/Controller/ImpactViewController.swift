66e24dece782dfa597c2cb3ed3dfb55a5c0876ab
Kaylie Hollander <Kphollander1@gmail.com>
2019-01-17 08:48:26 -0700

    Initial commit

---
+++ MapApp/Controller/ImpactViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  MapApp
+//
+//  Created by Hollander, Kaylie on 1/10/19.
+//  Copyright © 2019 Hollander, Kaylie. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ImpactViewController: UIViewController
+{
+        
+        
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    
+    @IBOutlet weak var animateButton: UIButton!
+    
+    @IBOutlet weak var impactButton: UIImageView!
+    override func viewDidLoad()
+        {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+        }
+        
+    @IBAction func animateClick(_ sender: UIButton)
+        {
+            let impactLocation = CLLocationCoordinate2D(latitude: -28.6657, longitude: 30.5296)
+            let mapSpan = MKCoordinateSpan(latitudeDelta: 10.05, longitudeDelta: 10.05)
+            let impactRegion = MKCoordinateRegion(center: impactLocation, span: mapSpan)
+            mapView.animatedZoom(zoomRegion: impactRegion, duration: 2.5)
+            
+        }
+    
+}
+
+extension MKMapView
+{
+    public func animatedZoom(zoomRegion: MKCoordinateRegion, duration: TimeInterval)
+    {
+        MKMapView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 10, options: UIView.AnimationOptions.curveEaseIn, animations:
+            {self.setRegion(zoomRegion, animated: true)}, completion: nil)
+    }
+}
